16c3e8f2374b5d37f167a4c3f82d72fc734fee40
AKingDebased <ggonzalv@lion.lmu.edu>
2014-09-17 20:08:24 -0700

    added maven file structure

---
+++ GeometryAPI/src/main/java/edu/lmu/cmsi/gabriel/geometryapi/CartesianPlane.java
@@ -0,0 +1,8 @@
+package edu.lmu.cmsi.gabriel.geometryapi;
+
+public class CartesianPlane{
+
+	public static void main(String[]args){
+		System.out.println("god?  are you there? it's me again");
+	}
+}
