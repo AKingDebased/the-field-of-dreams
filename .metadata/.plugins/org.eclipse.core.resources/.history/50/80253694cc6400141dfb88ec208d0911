0cc83cc82f83903dd958e0b80ef2bfcaf29e31bd
AKingDebased <ggonzalv@lion.lmu.edu>
2014-11-05 01:32:51 -0800

    added CCArrayIterator

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/50/80253694cc6400141dfb88ec208d0911
@@ -0,0 +1,5 @@
+package edu.lmu.cmsi.gabriel.customcollection;
+
+public class CCArrayTest {
+
+}
--- CustomCollection/src/test/java/edu/lmu/cmsi/gabriel/customcollection/CCArrayTest.java
+++ CustomCollection/src/test/java/edu/lmu/cmsi/gabriel/customcollection/CCArrayTest.java
@@ -1,5 +1,99 @@
 package edu.lmu.cmsi.gabriel.customcollection;
 
-public class CCArrayTest {
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import edu.lmu.cmsi.gabriel.customcollection.ccabstract.ccarray.CCArray;
 
+public class CCArrayTest {
+	
+	 @Test(expected = IllegalArgumentException.class)
+	 public void testNull() {
+		 CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		 testCCArray.add(null);
+	 }
+	 
+	 @Test
+	 public void lessThanNGetOldest(){
+		 CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		 
+		 fillCCArray(testCCArray, false);
+		 
+		 assertEquals("failed - expected 0", new Integer(0),testCCArray.getOldest());
+	 }
+	 
+	 @Test
+	 public void greaterThanNGetOldest(){
+		 CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		 
+		 fillCCArray(testCCArray, true);
+		 
+		 assertEquals("failed - expected 1", new Integer(1),testCCArray.getOldest());
+	 }
+	 
+	 @Test
+	 public void lessThanNGetNewest(){
+		 CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		 
+		 fillCCArray(testCCArray, false);
+		 
+		 assertEquals("failed - expected 2", new Integer(2),testCCArray.getNewest());
+	 }
+	 
+	 @Test
+	 public void greaterThanNGetNewest(){
+		 CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		 
+		 fillCCArray(testCCArray, true);
+		 
+		 assertEquals("failed - expected 4", new Integer(4),testCCArray.getNewest());
+	 }
+	 
+	 @Test
+	 public void sizeTest(){
+		 CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		 
+		 testCCArray.add(1);
+		 assertEquals("failed - expected 1", 1, testCCArray.getSize());
+		 
+		 testCCArray.add(2);
+		 testCCArray.add(3);
+		 assertEquals ("failed - expected 3", 3, testCCArray.getSize());
+		 
+		 testCCArray.add(4);
+		 assertEquals("failed - expected 4", 4, testCCArray.getSize());
+		 
+		 testCCArray.add(5); //exceeding defined CCArray size
+		 assertEquals("failed - expected 4", 4, testCCArray.getSize());
+	}
+	
+	@Test
+	public void sizeAndResetTest(){
+		CCArray<Integer> testCCArray = new CCArray<Integer>(4);
+		
+		fillCCArray(testCCArray, false);
+		testCCArray.reset();
+		 
+		assertEquals("failed - expected 0", 0, testCCArray.getSize());
+	}
+	
+	/*@Test
+	public void iterationOrderTest(){
+		CCArray<Integer> testCCArray = new CCArray<Integer>(15);
+		
+		for(int i = 0; i < 15; i++){
+			testCCArray.add(i);
+		}
+	}*/
+	
+	 private void fillCCArray(CCArray<Integer> CCArray, boolean moreOrLess){
+		 if(moreOrLess == false) { //false for < N
+			 for (int i = 0; i < 3; i++){
+				 CCArray.add(i);
+			 }
+		 } else {
+			 for (int i = 0; i < 5; i++){ //true for > N
+				 CCArray.add(i);
+			 }
+		 }
+	 }
 }
