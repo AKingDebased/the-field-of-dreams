a6bca4ac463eb2f1ff3143d95c29be26dd0dbda8
AKingDebased <ggonzalv@lion.lmu.edu>
2014-11-17 18:17:44 -0800

    updated README.md

--- RecursionAndStacks/src/main/java/edu/lmu/cmsi/gabriel/recursionandstacks/fibonaccirecursor/FibonacciRecursor.java
+++ RecursionAndStacks/src/main/java/edu/lmu/cmsi/gabriel/recursionandstacks/fibonaccirecursor/FibonacciRecursor.java
@@ -1,5 +1,13 @@
 package edu.lmu.cmsi.gabriel.recursionandstacks.fibonaccirecursor;
 
-public class FibonacciRecursor {
-
+public final class FibonacciRecursor {
+	
+	public static int fibonacci(int n)  {
+	    if(n == 0){
+	        return 0;
+	    } else if(n == 1){
+	      return 1;
+	    } 
+	      return fibonacci(n - 1) + fibonacci(n - 2);
+	}
 }
